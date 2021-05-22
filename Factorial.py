7a4c376b95df74fcd29bba5a93d9954784451a71
lewiscomba <66085548+lewiscomba@users.noreply.github.com>
2021-05-21 20:35:18 -0500

    my comment -no comment

---
+++ Factorial.py
@@ -0,0 +1,17 @@
+import getopt, sys
+
+
+
+
+# Ask the user to enter the names of files to compare
+number = input("Enter the a number: ")
+
+while 1 == 1:
+    if number != '':
+        prod = 1
+        for i in range(1,int(number)+1):
+            prod *= i
+        print('El numero factorial es: ', prod)
+        number = input("Enter the a number: ")
+    else:
+        break
